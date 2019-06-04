6345e057d8dae4eb275ed883dff787ae5fc97072
feiyao <764888648@qq.com>
2019-06-04 18:19:12 +0800

    初次联网

--- app/src/main/java/com/zfy/feifeiplayer/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/zfy/feifeiplayer/ui/fragment/HomeFragment.kt
@@ -1,24 +1,71 @@
 package com.zfy.feifeiplayer.ui.fragment
 
-import android.graphics.Color
-import android.view.Gravity
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.View
-import android.widget.TextView
+import com.zfy.feifeiplayer.R
+import com.zfy.feifeiplayer.adapter.HomeAdapter
 import com.zfy.feifeiplayer.base.BaseFragment
+import com.zfy.feifeiplayer.util.URLProviderUtils
+import kotlinx.android.synthetic.main.fragment_home.*
+import okhttp3.*
 import org.jetbrains.anko.debug
-import org.jetbrains.anko.textColor
+import java.io.IOException
+
 
 /**
  *
  */
-class HomeFragment: BaseFragment() {
-    override fun initView():View {
-        debug { "onCreateView->initListener" }
-        val tv = TextView(context)
-        tv.setText("我是首页")
-        tv.gravity = Gravity.CENTER
-        tv.textSize = 50f
-        tv.setTextColor(Color.BLACK)
-        return tv
+class HomeFragment : BaseFragment() {
+    override fun initView(): View {
+
+        return View.inflate(context, R.layout.fragment_home, null)
+    }
+
+    override fun init() {
+        super.init()
+
+    }
+
+    override fun initListener() {
+        super.initListener()
+        rcv.layoutManager = LinearLayoutManager(context)
+        var homeAdapter = HomeAdapter(context)
+        rcv.adapter = homeAdapter
+    }
+
+    override fun initData() {
+        super.initData()
+        getdata()
+    }
+
+    private fun getdata() {
+//        val homeUrl = URLProviderUtils.getHomeUrl(0, 20)
+        val homeUrl = "https://testapi.hngxsj.com/scsj-baseinfo/appStoreOperations/selectAppGoodsByStoreAndClass"
+
+        val okHttpClient = OkHttpClient()
+
+        val recontent = "{\"goodsClassId\":\"b5ca979634e611e99be17cd30adfe31a|04\",\"storeId\":\"b5ca979634e611e99be17cd30adfe31a\"}"
+        val requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"),recontent)
+        val token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNjYzODEzNzcyOCIsImF1ZGllbmNlIjoid2ViIiwiY3JlYXRlZCI6MTU1OTY0MTU2MTIwNSwiZXhwIjoxNTYwMjQ2MzYxfQ.BKP7Vx2RW_ehPk_KnDLrcDrsX61gfb3QmYTj6Lbb5NYitZDwyEAtYjMXxFJMOgENe-SLJ4jV4geRSqEeSGM3kQ"
+        val request = Request.Builder()
+                .url(homeUrl)
+                .addHeader("Authorization",token)
+                .post(requestBody)
+                .build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                debug { "网络请求失败==》"+e.message }
+                Log.e("zz","网络请求失败=2=》"+e.message)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                debug ( "网络请求成功==》"+response.toString() )
+                Log.e("zz","网络请求成功=2=》"+response.body()?.string())
+            }
+
+        })
+
+
     }
 }
