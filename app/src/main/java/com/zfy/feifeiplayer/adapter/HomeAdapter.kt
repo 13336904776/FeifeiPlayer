c98ee42f9830dc9921edbf975b2db073268611e5
feiyao <764888648@qq.com>
2019-06-10 19:02:46 +0800

    上拉加载更多

--- app/src/main/java/com/zfy/feifeiplayer/adapter/HomeAdapter.kt
+++ app/src/main/java/com/zfy/feifeiplayer/adapter/HomeAdapter.kt
@@ -1,18 +1,18 @@
 package com.zfy.feifeiplayer.adapter
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import com.zfy.feifeiplayer.bean.X
 import com.zfy.feifeiplayer.widget.HomeItemView
+import com.zfy.feifeiplayer.widget.LoadMoreView
 
 /**
  *
  */
-class HomeAdapter(mContext: Context?, var list: ArrayList<X>) : RecyclerView.Adapter<HomeAdapter.HomeHolder>() {
+class HomeAdapter() : RecyclerView.Adapter<HomeAdapter.HomeHolder>() {
 
-    //    var list = ArrayList<X>()
+    var list = ArrayList<X>()
     fun updata(mlist: List<X>) {
         list.clear()
         list.addAll(mlist)
@@ -24,22 +24,31 @@ class HomeAdapter(mContext: Context?, var list: ArrayList<X>) : RecyclerView.Ada
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): HomeHolder {
-
-        return HomeHolder(HomeItemView(p0.context))
+    override fun onCreateViewHolder(p0: ViewGroup, viewType: Int): HomeHolder {
+        if (viewType == 1) {
+            return HomeHolder(LoadMoreView(p0.context))
+        } else {
+            return HomeHolder(HomeItemView(p0.context))
+        }
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return list.size + 1
     }
 
     override fun onBindViewHolder(p0: HomeHolder, p1: Int) {
+        if (p1 == list.size) return
         val data = list[p1]
         val homeItemView = p0.itemView as HomeItemView
         homeItemView.setData(data)
     }
 
-    class HomeHolder(itemView: HomeItemView) : RecyclerView.ViewHolder(itemView) {
+    override fun getItemViewType(position: Int): Int {
+        if (position == list.size) return 1//1 加载更多
+        else return 0
+    }
+
+    class HomeHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
 }
--- app/src/main/java/com/zfy/feifeiplayer/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/zfy/feifeiplayer/ui/fragment/HomeFragment.kt
@@ -1,6 +1,9 @@
 package com.zfy.feifeiplayer.ui.fragment
 
+import android.graphics.Color
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import com.google.gson.Gson
@@ -10,7 +13,9 @@ import com.zfy.feifeiplayer.adapter.HomeAdapter
 import com.zfy.feifeiplayer.base.BaseFragment
 import com.zfy.feifeiplayer.bean.HomeBean
 import com.zfy.feifeiplayer.bean.X
+import com.zfy.feifeiplayer.util.ThreadUtil
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.Runnable
 import okhttp3.*
 import org.jetbrains.anko.debug
 import org.jetbrains.anko.support.v4.runOnUiThread
@@ -24,6 +29,8 @@ class HomeFragment : BaseFragment() {
 
     val pageSize = 10
     var pageNum = 1
+    var homeAdapter = HomeAdapter()
+    var isloadmore = false
 
     override fun initView(): View {
 
@@ -38,6 +45,33 @@ class HomeFragment : BaseFragment() {
     override fun initListener() {
         super.initListener()
         rcv.layoutManager = LinearLayoutManager(context)
+        rcv.adapter = homeAdapter
+        swr.setColorSchemeColors(Color.RED, Color.BLUE, Color.WHITE)
+
+        swr.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener {
+            pageNum = 1
+            isloadmore = false
+            getdata()
+        })
+
+        rcv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                when (newState) {
+                    RecyclerView.SCROLL_STATE_IDLE -> {
+                        val layoutManager = rcv.layoutManager
+                        if (layoutManager is LinearLayoutManager) {
+                            if (layoutManager.findLastVisibleItemPosition() == homeAdapter.list.size) {
+                                isloadmore = true
+                                getdata()
+                            }
+                        }
+                    }
+                    else -> {
+                    }
+                }
+            }
+        })
 
     }
 
@@ -47,6 +81,49 @@ class HomeFragment : BaseFragment() {
     }
 
     private fun getdata() {
+//        val homeUrl = URLProviderUtils.getHomeUrl(0, 20)
+        val homeUrl = "https://testapi.hngxsj.com/scsj-order/appShopOrder/getShopOrderListByTypeForBuyer"
+
+        val okHttpClient = OkHttpClient()
+        val recontent = "{\"pageInfo\":{\"pageNum\":$pageNum,\"pageSize\":$pageSize},\"type\":\"0\"}"
+
+        val requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), recontent)
+        val token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTAzNjE4Nzk2NSIsImF1ZGllbmNlIjoid2ViIiwiY3JlYXRlZCI6MTU1OTcyODMzNDAyOCwiZXhwIjoxNTYwMzMzMTM0fQ.2Wh15sscSRe2SmZTAMf_XQtPTtr_ZibstXNWkJSkvl7ARs8y4wweVe607_HkQsc3FwJqJLEhOJsminHKjAaDvw"
+        val request = Request.Builder()
+                .url(homeUrl)
+                .addHeader("Authorization", token)
+                .post(requestBody)
+//                .get()
+                .build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                ThreadUtil.runOnMainThread(object : Runnable {
+                    override fun run() {
+                        swr?.let { it.isRefreshing = false }
+                    }
+                })
+
+                debug { "网络请求失败==》" + e.message }
+                Log.e("zz", "网络请求失败=2=》" + e.message)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+                val result = response.body()?.string()
+                Log.e("zz", "网络请求成功=2=》" + result)
+                val homeBean = Gson().fromJson<HomeBean>(result, object : TypeToken<HomeBean>() {}.type)
+                runOnUiThread {
+                    //                    homeAdapter = HomeAdapter(context, homeBean.data.list as ArrayList<X>)
+                    swr?.let { it.isRefreshing = false }
+                    homeAdapter.updata(homeBean.data.list as ArrayList<X>)
+                }
+
+            }
+
+        })
+    }
+
+    private fun loadmore(pageNum: Int) {
 //        val homeUrl = URLProviderUtils.getHomeUrl(0, 20)
         val homeUrl = "https://testapi.hngxsj.com/scsj-order/appShopOrder/getShopOrderListByTypeForBuyer"
 
@@ -64,23 +141,34 @@ class HomeFragment : BaseFragment() {
                 .build()
         okHttpClient.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
+                ThreadUtil.runOnMainThread(object : Runnable {
+                    override fun run() {
+                        swr?.let { it.isRefreshing = false }
+                    }
+                })
+
                 debug { "网络请求失败==》" + e.message }
                 Log.e("zz", "网络请求失败=2=》" + e.message)
             }
 
             override fun onResponse(call: Call, response: Response) {
+
                 val result = response.body()?.string()
                 Log.e("zz", "网络请求成功=2=》" + result)
                 val homeBean = Gson().fromJson<HomeBean>(result, object : TypeToken<HomeBean>() {}.type)
                 runOnUiThread {
-                    var homeAdapter = HomeAdapter(context, homeBean.data.list as ArrayList<X>)
-                    rcv.adapter = homeAdapter
+
+                    swr?.let { it.isRefreshing = false }
+                    if (!isloadmore) {
+                        homeAdapter.updata(homeBean.data.list as ArrayList<X>)
+                    }else{
+                        homeAdapter.loadMore(homeBean.data.list as ArrayList<X>)
+                    }
                 }
 
             }
 
         })
-
 
     }
 }
